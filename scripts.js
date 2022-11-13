65dedb844929fa8cd2ca0d565787283092a33787
Mike Castro <mcastro@mcastro-Air.attlocal.net>
2022-11-13 14:27:23 -0600

    cool nav

---
+++ scripts.js
@@ -0,0 +1,11 @@
+const nav = document.getElementById("nav")
+
+for (const link of nav.getElementsByTagName('a')) {
+  link.onmousemove = e => {
+    const rect = link.getBoundingClientRect(),
+      img = link.querySelector('img')
+    
+    img.style.left = `${e.clientX - rect.left}px`;
+    img.style.top = `${e.clientY - rect.top}px`;
+  }
+}
